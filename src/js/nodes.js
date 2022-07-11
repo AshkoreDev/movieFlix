dd342543add68d43124b385763f258f2debf83d9
Aracely Alba <aracelyalba8@gmail.com>
2022-07-10 22:49:03 -0430

    Added get dom nodes

---
+++ src/js/nodes.js
@@ -0,0 +1,32 @@
+const nodes = (id) => document.querySelector(id);
+
+// MENU
+const home = $('#home');
+const search = $('#search');
+const categories = $('#categories');
+
+// SECTIONS
+const trendingSection = nodes('#trendingSection');
+const categorySection = nodes('#categorySection');
+const bySearchSection = nodes('#bySearchSection');
+const byCategorySection = nodes('#byCategorySection');
+const detailsCardSection = nodes('#detailsCardSection');
+
+// BUTTONS
+const backArrow = nodes('#backArrow');
+const searchBtn = nodes('#searchBtn');
+
+// TRENDING
+const trendingDayMoviesContainer = nodes('#trendingDayMoviesContainer');
+const trendingWeekMoviesContainer = nodes('#trendingWeekMoviesContainer');
+const trendingDayTvContainer = nodes('#trendingDayTvContainer');
+const trendingWeekTvContainer = nodes('#trendingWeekTvContainer');
+
+// CATEGORIES
+const categoriesContainer = nodes('#categoriesContainer');
+const byCategoriesContainer = nodes('#byCategoriesContainer');
+
+// SEARCH
+const searchTitle = nodes('#searchTitle');
+const searchInput = $('#searchInput');
+const bySearchContainer = nodes('#bySearchContainer');
